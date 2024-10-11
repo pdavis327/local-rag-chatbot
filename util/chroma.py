773d8cba7f9a2bbd2ffceb5c05bf31bad22bc9c1
Pete Davis <petedavis327@gmail.com>
2024-10-10 21:02:15 -0400

    Update chroma.py

--- util/chroma.py
+++ util/chroma.py
@@ -1,6 +1,7 @@
 import chromadb
 from langchain.vectorstores.chroma import Chroma
 import os
+from util import embedding
 
 def chroma_unique_id(data):
     data = data.to_pandas()
@@ -12,13 +13,7 @@ def query_database(collection, query_text, n_results=5):
     return results
 
 def upload_to_collection(collection_name, chunked_documents, embedding_model, persist_path = None):
-    # client = chromadb.Client()
-    # try:
-    #     collection = client.get_collection(name=collection_name)
-    #     print(f'collection already exists: {collection_name}')
-    # except:
-    #     print(f'collection doesnt exist, creating collection: {collection_name}')
-    #     collection = client.create_collection(collection_name)
+
     if persist_path:
         vectordb = Chroma.from_documents(
             collection_name = collection_name,
@@ -42,5 +37,24 @@ def get_vector_db(collection_name, embedding_model, persist_path):
         embedding_function=embedding_model
     )
 
-    return db
+
+if __name__ == "__main__":
+    # create chroma db of pdf documents
+    # user params
+    pdf_docs = ''
+    chroma_persist_path = 'db'
+    chroma_collection_name = 'disaster_response_collection_new'
+    embedding_model = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    # load pdf as langchain docs
+    docs = embedding.load_pdf_docs(pdf_docs)
+
+    # split and chunk the documents to prep for embedding
+    chunked = embedding.rec_split_chunk(docs, chunk_size = 512, chunk_overlap = 30)
+
+    # create persistent vector db of embeddings in chroma
+    db = upload_to_collection(collection_name=chroma_collection_name, 
+    chunked_documents= chunked, 
+    embedding_model= embedding_model,
+    persist_path = "db")
 
